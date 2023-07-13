391eb909eb3db185eec710848d82283bc3f5fea1
Amanda Simões <amandasimoesdev@gmail.com>
2023-07-13 17:47:14 -0300

    Two-way Data Binding / Router and RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listarPensamento',
+    pathMatch: 'full'
+
+  },
+  {
+    path:'listarPensamento',
+    component: ListarPensamentoComponent
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentosComponent
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
